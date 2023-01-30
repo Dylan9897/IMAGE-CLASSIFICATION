20aafd31eb46003c659f7995b8d6888bf6e58abc
Dylan9897 <handong_xu@163.com>
2023-01-30 11:05:20 +0800

    update AlexNet Vgg16

--- models/modelBase.py
+++ models/modelBase.py
@@ -1,6 +1,6 @@
 import torch
-import torchsummary
-from models.Mish import Mish
+# import torchsummary
+from module.Mish import Mish
 
 class Conv_Block(torch.nn.Module):
     def __init__(self,in_channels,out_channels,kernel_size,stride,padding):
@@ -40,33 +40,3 @@ class Reshape_Block(torch.nn.Module):
     def forward(self,x):
         out = x.reshape(self.shape)
         return out
-
-
-class Model_NN(torch.nn.Module):
-    def __init__(self,arch,model_name=None):
-        super(Model_NN,self).__init__()
-        self.arch = arch
-        self.model = self.getModel()
-    
-    def getModel(self):
-        layers=[]
-        for L in self.arch:
-            if L[0]=="Conv2D":
-                layers.append(Conv_Block(L[1],L[2],L[3],L[4],L[5]))
-            elif L[0]=="Maxpool":
-                layers.append(torch.nn.MaxPool2d(L[1],L[2],L[3]))
-            elif L[0]=="Reshape":
-                layers.append(Reshape_Block(L[1]))
-            elif L[0]=="Conn":
-                layers.append(Linear_Block(L[1],L[2]))
-            elif L[0]=="LastLinear":
-                layers.append(Linear_Block(L[1],L[2],tag=False))
-        return torch.nn.Sequential(*layers)
-    
-    def forward(self,x):
-        out = self.model(x)
-        return out
-
-def Model(config,device):
-    model = Model_NN(config["params"],config["model"]).to(device)
-    return model
